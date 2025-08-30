fd8874f5b18e1e283ffcc7dff6fbab24c7ef7cd2
mateus_wendhausen <mateusdwendhausen@gmail.com>
2025-08-29 21:42:42 -0300

    projeto comit

--- rock/src/main/java/com/poo/rock/RockApplication.java
+++ rock/src/main/java/com/poo/rock/RockApplication.java
@@ -2,13 +2,29 @@ package com.poo.rock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@
+@RestController
 public class RockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RockApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    /*
+    HTTP <> != HTTPS
+    Hyper Text Transfer Protocol
+    GET Buscar as informações
+    POST Enviar as informações ou empurrar
+    PUT Alterar as informações ou mudar
+    PATCH Alterar as informações do campo consultas
+    DELETE Deletar as informações ou apagar
+     */
 
 }
